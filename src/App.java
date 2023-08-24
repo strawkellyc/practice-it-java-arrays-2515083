000df972cc3bbca54132db00a58f00a6285e07f4
KellyC <127417904+strawkellyc@users.noreply.github.com>
2023-08-24 21:34:39 +0000

    Ans_rotate an array

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class App {
 
-  public static Object[] rotateRight(Object[] arr) {
+  /* public static Object[] rotateRight(Object[] arr) {
     Object current = 0;
     for (int i = arr.length -1; i > 0 && i <= arr.length - 1; i--) {
       current = arr[i];
@@ -11,7 +11,29 @@ public class App {
 
     }
 
-    return arr;
+    return arr; 
+    */
+
+    public static Object[] rotateRight(Object[] arr) {
+      if (arr == null) {
+        return null;
+      }
+      
+      Object[] result = new Object[arr.length];
+      // Result[] -> {null, null, null, null, null}
+      for (int i = 0; i < arr.length; i++) {
+        // arr[] -> {1, 2, 3, 4, 5}
+        
+        // failure
+        // result[] -> {null, 1, 2, 3, 4} 
+        // result[i + 1] = arr[i];   --> to prevent invalid index, this is not how it works
+        
+        // success
+        // result[] -> {5, 1, 2, 3, 4}
+        result[(i+1) % result.length] = arr[i];
+
+      }
+      return result;
   }
 
   public static void main(String args[]) {
