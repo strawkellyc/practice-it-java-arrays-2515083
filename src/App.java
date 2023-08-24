b8ee3c8e8fcbb6f59f048f956b635ba2160c39c5
KellyC <127417904+strawkellyc@users.noreply.github.com>
2023-08-24 20:44:17 +0000

    K_rotate an array

--- src/App.java
+++ src/App.java
@@ -1,49 +1,40 @@
+import java.util.Arrays;
+
 public class App {
 
-  public static void printTriangle(Object[] arr) {
-     
-    for (int i = 0; i < arr.length; i++) {
-      //if (arr.length < 1) {  --> meaningless, coz if arr.length is 0, can't 
-      //  System.out.println();  --> get through the for loop
-     // } 
-        if (arr.length <= 1 + i) 
-        {for (int j = 0; j <= i; j++) {
-          for (int x = 0; x < 1 + j; x++) {
-            System.out.print(arr[x]);
-          }
-          System.out.println();
-        }
-        
+  public static Object[] rotateRight(Object[] arr) {
+    Object current = 0;
+    for (int i = arr.length -1; i > 0 && i <= arr.length - 1; i--) {
+      current = arr[i];
+      arr[i] = arr[i - 1];
+      arr[i - 1] = current;
 
-        
-        }}
-      System.out.println();
     }
 
-
-    // {A, B, C}
-    // A
-    // AB
-    // ABC
-/* 
-    for (int row = 0; row < arr.length; row++) {
-      for (int col = 0; col <= row; col++) {
-        System.out.print(arr[col]);  
-        }
-        System.out.println();
-      }
-      System.out.println();
-    }
-*/
-  
-
-  // System.out.println(arr);
+    return arr;
+  }
 
   public static void main(String args[]) {
-    printTriangle(new Integer[] { 1, 2, 3, 4 });
-    printTriangle(new Character[] { 'A', 'B', 'C', 'D', 'E' });
-    printTriangle(new String[] {});
-    printTriangle(new String[] { "Hello" });
-    printTriangle(new String[] { "Hello", "There", "Friend" });
+    Object[] arr = new Integer[] { 1, 2, 3, 4, 5 };
+
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+
+    Object[] arr2 = new String[] { "Hello", "there", "my friend" };
+
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
   }
 }
